8198a58e1103ffc92a48135bb101d42a64859ac9
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-28 07:45:55 +0000

    second add

---
+++ cards.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  char card_name[3];
+  puts("Please enter card's name");
+  scanf("%2s",card_name);
+  int val = 0;
+  if(card_name[0] == 'k'){
+    val = 10;
+  }else if(card_name[0] == 'Q'){
+    val = 10;
+  }else if (card_name[0] == 'J'){
+    val = 10;
+  }else if (card_name[0] == 'A'){
+    val = 11;
+  }else {
+    val = atoi(card_name);
+  }
+
+  if((2 < val) && (val < 7))
+    puts("count  plus.");
+  else if (val == 10)
+    puts("count minus.");
+
+  return 0;
+}
