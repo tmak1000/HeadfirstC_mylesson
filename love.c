782c8f45ad4e98966a59517e2a3df96bc7bf207c
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-28 12:19:05 +0000

    first add

---
+++ love.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int contestants[] = {1,2,3};
+  int *choice = contestants;
+  contestants[0] = 2;
+  contestants[1] = contestants[2];
+  contestants[2] = *choice;
+  printf("I select number %i\n",  contestants[2]);
+  return 0;
+}
