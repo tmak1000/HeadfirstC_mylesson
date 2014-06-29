cb5c73e4d4c4a421848b0804efa08af3e8689418
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-29 04:20:08 +0000

    first add

---
+++ print_reverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+void print_reverse(char *s)
+{
+  size_t len = strlen(s);
+  char *t = s + len -1;
+  while(t >= s){
+    printf("%c",*t);
+    t = t - 1;
+    }
+  puts("");
+}
+
+int main(){
+  char s[] = "ABC";
+  print_reverse(s);
+  return 0;
+}
