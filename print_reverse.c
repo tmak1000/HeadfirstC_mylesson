c5d80ced7767cd7bfd6055af14a4f67d480393ca
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-06-29 04:32:05 +0000

    second commit

--- print_reverse.c
+++ print_reverse.c
@@ -13,7 +13,25 @@ void print_reverse(char *s)
 }
 
 int main(){
-  char s[] = "ABC";
-  print_reverse(s);
+  //  char s[] = "ABC";
+  //  print_reverse(s);
+
+  char *juices[] = {"dragonfruit","waterberry","sharonfruit",
+		    "uglifruit","rumberry","kiwifruit","mulberry","strawberry",
+		    "blueberry","blackberry","starfruit"
+  };
+  char *a;
+  puts(juices[6]);
+  print_reverse(juices[7]);
+  a = juices[2];
+  juices[2] = juices[8];
+  juices[8] = a;
+  puts(juices[8]);
+  print_reverse(juices[(18+7)/5]);
+  puts(juices[2]);
+  print_reverse(juices[9]);
+  juices[1] = juices[3];
+  puts(juices[10]);
+  print_reverse(juices[1]);
   return 0;
 }
