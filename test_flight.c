ca332313fc804f7637fdaf9ffa88d2fa6f355209
tmak1000 <watanabe4919@mx36.tiki.ne.jp>
2014-07-06 12:41:50 +0000

    first add

---
+++ test_flight.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+typedef struct island{
+  char *name;
+  char *opens;
+  char *closes;
+  struct island *next;
+}island;
+
+void display(island *start){
+  island *i = start;
+  for(;i != NULL;i = i->next){
+    printf("name : %s time:%s-%s\n",i->name,i->opens,i->closes);
+  }
+}
+
+island * create(char *name)
+{
+  island *i = malloc(sizeof(island));
+  i->name = strdup(name);
+  i->opens = "09:00";
+  i->closes = "17:00";
+  i->next = NULL;
+  return i;
+}
+
+int main(){
+
+  char name[80];
+  fgets(name,80,stdin);
+  island *p_island0 = create(name);
+
+  fgets(name,80,stdin);
+  island *p_island1 = create(name);
+  p_island0->next = p_island1;
+  display(p_island0);
+
+  return 0;
+}
